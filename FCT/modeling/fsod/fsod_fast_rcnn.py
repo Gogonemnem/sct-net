d2f787b61109c86d38c197a957e7ffc0a31c668a
Gonem <glau.github@pm.me>
2024-07-26 12:00:46 +0200

    fix: detection loss - correct sorting/slicing, based on batch size

--- FCT/modeling/fsod/fsod_fast_rcnn.py
+++ FCT/modeling/fsod/fsod_fast_rcnn.py
@@ -2,7 +2,7 @@ from typing import Callable, Dict, List, Optional, Tuple, Union
 import torch
 from torch.nn import functional as F
 
-# from detectron2.config import configurable
+from detectron2.config import configurable
 from detectron2.layers import cat, cross_entropy
 from detectron2.structures import Instances
 from detectron2.modeling.roi_heads.fast_rcnn import FastRCNNOutputLayers, _log_classification_stats, fast_rcnn_inference
@@ -12,8 +12,20 @@ __all__ = ["FsodFastRCNNOutputLayers"]
 
 
 class FsodFastRCNNOutputLayers(FastRCNNOutputLayers):
+    @configurable
+    def __init__(self, num_proposals, **kwargs):
+        super().__init__(**kwargs)
+        self.num_proposals = num_proposals
+    
+    @classmethod
+    def from_config(cls, cfg, input_shape):
+        ret = super().from_config(cfg, input_shape)
+        ret['num_proposals'] = cfg.MODEL.ROI_HEADS.BATCH_SIZE_PER_IMAGE
+        return ret
+
     def forward(self, x, y=None):
-        return super().forward(x)
+        x = super().forward(x)
+        return x
 
     def losses(self, predictions: Tuple[torch.Tensor, torch.Tensor], proposals: List[Instances]):
         """
@@ -43,55 +55,53 @@ class FsodFastRCNNOutputLayers(FastRCNNOutputLayers):
         else:
             proposal_boxes = gt_boxes = torch.empty((0, 4), device=proposal_deltas.device)
 
-        return {
+        losses =  {
             "loss_cls": self.softmax_cross_entropy_loss(scores, gt_classes),
             "loss_box_reg": self.box_reg_loss(
                 proposal_boxes, gt_boxes, proposal_deltas, gt_classes
             ),
         }
-    
+        return {k: v * self.loss_weight.get(k, 1.0) for k, v in losses.items()}
+
     def softmax_cross_entropy_loss(self, scores: torch.Tensor, gt_classes: torch.Tensor):
         """
         Compute the softmax cross entropy loss for box classification.
         Returns:
             scalar Tensor
         """
+        n_instances_per_class = self.num_proposals
+
         # TODO: use configs to figure it out
-        n_instances_per_class = 128 # self.gt_classes.numel()?
         background_ratio = 2
         negative_ratio = 1
-
-        cls_score_softmax = F.softmax(scores, dim=1)
 
         fg_inds = (gt_classes == 0).nonzero().squeeze(-1)
         bg_inds = (gt_classes == 1).nonzero().squeeze(-1)
 
-        
-        bg_cls_score_softmax = cls_score_softmax[bg_inds, :]
-
         n_positives_foreground = fg_inds.shape[0]
-        n_positives_background = n_positives_foreground * background_ratio
-        n_negatives = n_positives_foreground * negative_ratio
-
-        if  n_positives_background + n_positives_foreground > n_instances_per_class:
-            n_positives_background = n_instances_per_class - n_positives_foreground
-
-            # anchor the ratio to positive background, same logic as in the original implementation
-            # maybe not necessary as the ratio should be anchored to positive foreground?
-            # n_negatives = n_positives_background
-        
-        # TODO: unnecessary prior calculations if n_positives_foreground == 0
         if n_positives_foreground == 0:
             n_positives_background = 1
             n_negatives = 1
+        else:
+            n_positives_background = n_positives_foreground * background_ratio
+            n_negatives = n_positives_foreground * negative_ratio
 
-        # TODO: check original implementation if this is correct (premature sorting?)
-        sorted, sorted_bg_inds = torch.sort(bg_cls_score_softmax[:, 0], descending=True)
-        real_bg_inds        = bg_inds[sorted_bg_inds]
-        real_bg_topk_inds_0 = real_bg_inds[real_bg_inds <  n_instances_per_class][:n_positives_background]
-        real_bg_topk_inds_1 = real_bg_inds[real_bg_inds >= n_instances_per_class][:n_negatives]
+            if  n_positives_background + n_positives_foreground > n_instances_per_class:
+                n_positives_background = n_instances_per_class - n_positives_foreground
 
-        topk_inds = torch.cat([fg_inds, real_bg_topk_inds_0, real_bg_topk_inds_1], dim=0)
+                # anchor the ratio to positive background, same logic as in the original implementation
+                # maybe not necessary as the ratio should be anchored to positive foreground?
+                # n_negatives = n_positives_background
+
+        normed_scores = F.softmax(scores, dim=1)
+        bg_cls_scores = normed_scores[bg_inds, 0]
+
+        _, sorted_bg_inds = torch.sort(bg_cls_scores, descending=True)
+        sorted_bg_inds        = bg_inds[sorted_bg_inds]
+        pos_bg_inds = sorted_bg_inds[:n_positives_background]
+        neg_bg_inds = sorted_bg_inds[n_instances_per_class - n_positives_foreground:n_instances_per_class - n_positives_foreground + n_negatives]
+
+        topk_inds = torch.cat([fg_inds, pos_bg_inds, neg_bg_inds], dim=0)
 
         if self.use_sigmoid_ce:
             loss_cls = self.sigmoid_cross_entropy_loss(scores[topk_inds], gt_classes[topk_inds])
