7ff30c849690150b00060b0fdb5c31bbd90b9e56
Gogonemnem <glau.github@pm.me>
2024-07-31 16:36:11 +0200

    fix: remove one false xqsa config

--- scripts/train_all.sh
+++ scripts/train_all.sh
@@ -13,13 +13,14 @@ for dataset in "${datasets[@]}"; do
 done
 
 # XQSA, only if pretraining is done
-# few_shot_configs=("pvt_v2/pvtv2-xqsa-pvt5" "pvt_v2/pvtv2-retinanet-xqsa-multirelation" "pvt_v2/pvtv2-retinanet-xqsa-pvt5")
-# stages="two-branch,fine-tuning"
+base_configs=("pvt_v2/pvtv2-retinanet" "pvt_v2/pvtv2-retinanet-pvt5")
+few_shot_configs=("pvt_v2/pvtv2-retinanet-xqsa-multirelation" "pvt_v2/pvtv2-retinanet-xqsa-pvt5")
+stages="two-branch,fine-tuning"
 
-# for dataset in "${datasets[@]}"; do
-#     for i in "${!base_configs[@]}"; do
-#         base_config="${base_configs[i]}"
-#         few_shot_config="${few_shot_configs[i]}"
-#         sbatch scripts/train.slurm --dataset "$dataset" --base-config "$base_config" --few-shot-config "$few_shot_config" --stages "$stages"
-#     done
-# done
+for dataset in "${datasets[@]}"; do
+    for i in "${!base_configs[@]}"; do
+        base_config="${base_configs[i]}"
+        few_shot_config="${few_shot_configs[i]}"
+        sbatch scripts/train.slurm --dataset "$dataset" --base-config "$base_config" --few-shot-config "$few_shot_config" --stages "$stages"
+    done
+done
